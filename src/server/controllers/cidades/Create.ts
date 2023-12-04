97989da5aee3fb8a783a143b6f856a85f63ea5b9
Reis567 <matheusdosreis9@gmail.com>
2023-12-04 08:01:52 -0300

    initial data validation with yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup'
 
-export const create = (req:Request, res:Response)=>{
-    return res.send('Botafogo')
+const bodyValidation = yup.object({
+    nome: yup.string().required().min(3),
+  });
+  
+interface ICidade extends yup.InferType<typeof bodyValidation> {
+    nome:string
+  }
+
+export const create = async (req:Request<{},{},ICidade>, res:Response)=>{
+
+    try {
+        await bodyValidation.validate(req.body)
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(req.body.nome)
+    return res.send(req.body)
 };
