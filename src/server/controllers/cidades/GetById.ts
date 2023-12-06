a8a6aed391ccd609ad023ad7ee132ccaad81a22a
Reis567 <matheusdosreis9@gmail.com>
2023-12-06 07:37:31 -0300

    getbyid controller

---
+++ src/server/controllers/cidades/GetById.ts
@@ -0,0 +1,26 @@
+
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup'
+import { validation } from "../../shared/middlewares";
+
+const paramsValidation = yup.object({
+    id: yup.number().integer().required().moreThan(0),
+  });
+  
+interface IParamsProps extends yup.InferType<typeof paramsValidation> {
+    id:number;
+  }
+  
+
+
+export const getAllValidation = validation((getSchema)=>({
+    params:getSchema<IParamsProps>(paramsValidation),
+}));
+
+
+export const getById = async (req:Request<IParamsProps>, res:Response)=>{
+    console.log(req.params)
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Ainda não implementado')
+};
