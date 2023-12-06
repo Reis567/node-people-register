ee420cd73ca72d8372b4a036911fd70eac08a167
Reis567 <matheusdosreis9@gmail.com>
2023-12-06 18:29:31 -0300

    organization and comments in routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,14 +4,19 @@ import { CidadesController } from "../controllers";
 
 const router = Router()
 
-
+//Base routes
 router.get("/",)
 
+//Read routes
 router.get("/cidades",CidadesController.getAllValidation,CidadesController.getAll);
 router.get("/cidades/:id",CidadesController.getByIdValidation,CidadesController.getById);
 
+
+//Create routes
 router.post("/cidades",CidadesController.createValidation,CidadesController.create)
 
+
+//Update routes
 router.put("/cidades/:id",CidadesController.UpdateByIdValidation,CidadesController.UpdateById)
 
 
