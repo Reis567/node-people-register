4b770e05b4e16601fb8bb06fa6bb73de92a6fbfc
Reis567 <matheusdosreis9@gmail.com>
2023-12-07 15:04:42 -0300

    created status code

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -22,5 +22,5 @@ export const createValidation = validation((getSchema)=>({
 export const create:RequestHandler  = async (req:Request<{},{},ICidade>, res:Response)=>{
     console.log(req.body)
 
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Ainda não implementado')
+    return res.status(StatusCodes.CREATED).send('Ainda não implementado')
 };
