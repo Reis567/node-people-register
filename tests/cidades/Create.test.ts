0c4aef8d31041eeed556e4581a9d956a2e759fab
Reis567 <matheusdosreis9@gmail.com>
2023-12-07 08:14:58 -0300

    test of tiny name

--- tests/cidades/Create.test.ts
+++ tests/cidades/Create.test.ts
@@ -9,5 +9,15 @@ describe('Cidades - Create', ()=>{
         
         })
         expect(resposta1.statusCode).toEqual(StatusCodes.CREATED);
+        expect(typeof resposta1.body).toEqual('number');
+    });
+    it('Tenta Criar registro com nome curto',async ()=>{
+        const resposta1 = await testServer.post('/cidades').send({
+        
+            nome:'Ma',
+        
+        })
+        expect(resposta1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(resposta1.body).toHaveProperty('errors.body.nome');
     });
 });
