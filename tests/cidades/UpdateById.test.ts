2e79a982a0bf191c3a2860a4abbb21b5c3f8d815
Reis567 <matheusdosreis9@gmail.com>
2023-12-08 17:45:44 -0300

    updatebyid first  test

--- tests/cidades/UpdateById.test.ts
+++ tests/cidades/UpdateById.test.ts
@@ -1,2 +1,12 @@
 import { testServer } from './../jest.setup';
 import { StatusCodes } from 'http-status-codes';
+
+it('Tenta editar registro inexistente', async () => {
+
+    const response = await testServer.put(`/cidades/99999`).send({
+        nome:'Maricá'
+    });
+
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.body).toHaveProperty('errors.default');
+  });
