6560a6f873aada3c47102095d6d83c4564e45c75
Reis567 <matheusdosreis9@gmail.com>
2023-12-08 18:01:35 -0300

    update by id second test

--- tests/cidades/UpdateById.test.ts
+++ tests/cidades/UpdateById.test.ts
@@ -1,12 +1,29 @@
 import { testServer } from './../jest.setup';
 import { StatusCodes } from 'http-status-codes';
+describe('Cidades - GetById',()=>{
+    it('Edita registro existente', async () => {
+        const resCria = await testServer.post('/cidades').send({
+            nome: 'Maricá',
+          });
+          
+          expect(resCria.statusCode).toEqual(StatusCodes.CREATED);
+          
+          const resAtualiza = await testServer.put(`/cidades/${resCria.body}`).send({
+            nome:'Marica2'
+          });
+          
+          expect(resAtualiza.statusCode).toEqual(StatusCodes.NO_CONTENT);
+          
+      });
+    
+    it('Tenta editar registro inexistente', async () => {
+    
+        const response = await testServer.put(`/cidades/99999`).send({
+            nome:'Maricá'
+        });
+    
+        expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+        expect(response.body).toHaveProperty('errors.default');
+      });
 
-it('Tenta editar registro inexistente', async () => {
-
-    const response = await testServer.put(`/cidades/99999`).send({
-        nome:'Maricá'
-    });
-
-    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
-    expect(response.body).toHaveProperty('errors.default');
-  });
+})
