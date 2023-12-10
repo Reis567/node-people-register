1d5f7188145e582209c83a870fefd6501937392b
Reis567 <matheusdosreis9@gmail.com>
2023-12-10 18:02:01 -0300

    controller interface changing to the model interface

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,23 +3,23 @@ import { Request, RequestHandler, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup'
 import { validation } from "../../shared/middlewares";
+import { ICidade } from "../../database/models";
+
 
 const bodyValidation = yup.object({
     nome: yup.string().required().min(3),
   });
-  
-interface ICidade extends yup.InferType<typeof bodyValidation> {
-    nome:string,
-  }
+
+interface IBodyProps extends Omit<ICidade,'id'>{ }
   
 
 
 export const createValidation = validation((getSchema)=>({
-    body:getSchema<ICidade>(bodyValidation),
+    body:getSchema<IBodyProps>(bodyValidation),
 }));
 
 
-export const create:RequestHandler  = async (req:Request<{},{},ICidade>, res:Response)=>{
+export const create:RequestHandler  = async (req:Request<{},{},IBodyProps>, res:Response)=>{
     console.log(req.body)
 
     return res.status(StatusCodes.CREATED).json(1);
