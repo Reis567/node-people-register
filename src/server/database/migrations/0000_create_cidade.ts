f3056e6173eae67f3611375345dcb119966f4537
Reis567 <matheusdosreis9@gmail.com>
2023-12-10 13:23:13 -0300

    table names file

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+
+export async function up(knex: Knex){
+    return knex.schema.createTable(ETableNames.cidade,table=>{
+       table.bigIncrements('id').primary().index();
+       table.string('nome',150).index().notNullable()
+
+       table.comment('Tabela usada para armazenar cidades no sistema')
+    }).then(()=>{
+        console.log(`# Create Table ${ETableNames.cidade}`)
+    })
+}
+
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable(ETableNames.cidade)
+    .then(()=>{
+        console.log(`# Dropped Table ${ETableNames.cidade}`)
+    })
+}
+
