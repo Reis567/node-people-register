afa3f0fafbe3906c3b3e5618d258cb207e8f8946
Reis567 <matheusdosreis9@gmail.com>
2023-12-10 11:14:51 -0300

    seeds directory

--- src/server/database/knex/Enviroment.ts
+++ src/server/database/knex/Enviroment.ts
@@ -9,7 +9,10 @@ export const development:Knex.Config = {
         filename: path.resolve(__dirname,'..','..','..','..','database.sqlite')
     },
     migrations:{
-        directory:path.resolve(__dirname,'..'),
+        directory:path.resolve(__dirname,'..','migrations'),
+    },
+    seeds:{
+        directory:path.resolve(__dirname,'..','seeds'),
     }
 }
 
