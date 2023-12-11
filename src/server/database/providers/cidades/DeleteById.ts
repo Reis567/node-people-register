aaa3789d10280ce6ba5ffd3190cb4db799966b8c
Reis567 <matheusdosreis9@gmail.com>
2023-12-11 17:42:19 -0300

    Delete By ID provider

---
+++ src/server/database/providers/cidades/DeleteById.ts
@@ -0,0 +1,17 @@
+import { ETableNames } from "../../ETableNames";
+import { Knex } from "../../knex";
+
+export const deleteById = async (id: number): Promise<void | Error> => {
+    try {
+        const result = await Knex(ETableNames.cidade).where({ id }).del();
+
+        if (result === 0) {
+            throw new Error('Registro não encontrado');
+        }
+
+        console.log(`Registro com ID ${id} excluído com sucesso.`);
+    } catch (error) {
+        console.error('Erro ao excluir o registro:', error);
+        return new Error('Erro ao excluir o registro!');
+    }
+};
--- src/server/database/providers/cidades/index.ts
+++ src/server/database/providers/cidades/index.ts
@@ -2,12 +2,12 @@
 import * as create from "./Create"
 //import * as getById from './GetById'
 //import * as UpdateById from './UpdateById'
-//import * as DeleteById from './DeleteById'
+import * as deleteById from './DeleteById'
 
 export const CidadesProvider = {
     ...create,
 //    ...getAll,
 //    ...getById,
 //    ...UpdateById,
-//    ...DeleteById,
+    ...deleteById,
 }
