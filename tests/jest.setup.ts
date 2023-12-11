9d1e29b7691bdcae8b0aa04f8e3d7fc5efeafbfd
Reis567 <matheusdosreis9@gmail.com>
2023-12-11 07:44:42 -0300

    test db migrate on jest

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,4 +1,11 @@
 import supertest from "supertest";
 import { server } from "../src/server/server";
+import { Knex } from "../src/server/database/knex";
+
+
+beforeAll(async()=>{
+    await Knex.migrate.latest();
+});
+
 
 export const testServer = supertest(server)
