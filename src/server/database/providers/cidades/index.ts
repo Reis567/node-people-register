9e71ec037bec1ce4290afdf56935bf638e4278ff
Reis567 <matheusdosreis9@gmail.com>
2023-12-11 06:57:36 -0300

    index of providers

---
+++ src/server/database/providers/cidades/index.ts
@@ -0,0 +1,13 @@
+//import * as getAll  from './GetAll';
+import * as create from "./Create"
+//import * as getById from './GetById'
+//import * as UpdateById from './UpdateById'
+//import * as DeleteById from './DeleteById'
+
+export const CidadesController = {
+    ...create,
+//    ...getAll,
+//    ...getById,
+//    ...UpdateById,
+//    ...DeleteById,
+}
