32182006952d8162ba93e4e87b0a574a191f24dc
Reis567 <matheusdosreis9@gmail.com>
2023-12-12 15:44:39 -0300

    if else to check production or development

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ const startServer = () =>{
     server.listen(process.env.PORT || 3333,()=>console.log(`App rodando na porta - ${process.env.PORT || 3333} `))
 }
 
+if(process.env.IS_LOCALHOST !== 'true'){
 
-Knex.migrate.latest().then(()=>{
-    startServer();
-})
+    Knex.migrate.latest().then(()=>{
+        startServer();
+    })
+    .catch(console.log)
+    
+}else{
+    startServer()
+}
