8a19175e256884309b42e825670a54e9ec32a407
Reis567 <matheusdosreis9@gmail.com>
2023-12-12 15:38:06 -0300

    migrations on server up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/server";
 
 
-console.log(`http://localhost:${process.env.PORT || 3333}`)
+const startServer = () =>{
 
-server.listen(process.env.PORT || 3333,()=>console.log(`App rodando na porta - ${process.env.PORT || 3333} `))
+    console.log(`http://localhost:${process.env.PORT || 3333}`)
+    
+    server.listen(process.env.PORT || 3333,()=>console.log(`App rodando na porta - ${process.env.PORT || 3333} `))
+}
+
+
+Knex.migrate.latest().then(()=>{
+    startServer();
+})
