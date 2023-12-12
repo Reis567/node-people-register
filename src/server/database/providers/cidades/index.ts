697e109e4c7f6932ccf688b29c9120e6b9918a9a
Reis567 <matheusdosreis9@gmail.com>
2023-12-12 07:13:13 -0300

    Cidades provider methods export

--- src/server/database/providers/cidades/index.ts
+++ src/server/database/providers/cidades/index.ts
@@ -1,13 +1,13 @@
-//import * as getAll  from './GetAll';
+import * as getAll  from './GetAll';
 import * as create from "./Create"
-//import * as getById from './GetById'
-//import * as UpdateById from './UpdateById'
+import * as getById from './GetById'
+import * as UpdateById from './UpdateById'
 import * as deleteById from './DeleteById'
 
 export const CidadesProvider = {
     ...create,
-//    ...getAll,
-//    ...getById,
-//    ...UpdateById,
+    ...getAll,
+    ...getById,
+    ...UpdateById,
     ...deleteById,
 }
