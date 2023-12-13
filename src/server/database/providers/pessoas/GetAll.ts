3653db59995df98e3ee5e7c62bb9bc15ab578501
Reis567 <matheusdosreis9@gmail.com>
2023-12-13 07:41:49 -0300

    git add . changes in get all

--- src/server/database/providers/pessoas/GetAll.ts
+++ src/server/database/providers/pessoas/GetAll.ts
@@ -2,27 +2,16 @@ import { ETableNames } from "../../ETableNames";
 import { Knex } from "../../knex";
 import { IPessoa } from "../../models";
 
-export const getAll = async (page: number, limit: number, filter: string, id = 0): Promise<IPessoa[] | Error> => {
+export const getAll = async (page: number, limit: number, filter: string): Promise<IPessoa[] | Error> => {
     try {
         let query = Knex(ETableNames.pessoa)
             .select('*')
-            .where('id',Number(id))
-            .orWhere('nomeCompleto', 'like', `%${filter}%`)
+            .where('nomeCompleto', 'like', `%${filter}%`)
             .offset((page - 1) * limit)
             .limit(limit);
 
         const result = await query;
 
-        if (id > 0 && result.every(item => item.id !== id)) {
-            const resultById = await Knex(ETableNames.pessoa)
-                .select('*')
-                .where('id','=',id)
-                .first();
-
-            if (resultById) {
-                return [...result, resultById];
-            }
-        }
 
         return result;
     } catch (error) {
