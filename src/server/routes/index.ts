dfd3345342833d50b276e3ce455c957d50d8324f
Reis567 <matheusdosreis9@gmail.com>
2023-12-13 18:13:55 -0300

    read routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,27 +1,28 @@
 import { Router } from "express";
 import {StatusCodes} from "http-status-codes"
-import { CidadesController } from "../controllers";
+import { CidadesController, PessoasController } from "../controllers";
 
 const router = Router()
 
 //Base routes
 router.get("/",)
 
+// Rotas de Cidades
+
 //Read routes
 router.get("/cidades",CidadesController.getAllValidation,CidadesController.getAll);
 router.get("/cidades/:id",CidadesController.getByIdValidation,CidadesController.getById);
-
-
 //Create routes
 router.post("/cidades",CidadesController.createValidation,CidadesController.create)
-
-
 //Update routes
 router.put("/cidades/:id",CidadesController.UpdateByIdValidation,CidadesController.UpdateById)
-
 //Delete routes
 router.delete("/cidades/:id",CidadesController.DeleteByIdValidation,CidadesController.DeleteById)
 
+// Rotas de Pessoas
 
+//Read routes
+router.get("/pessoas",PessoasController.getAllValidation,PessoasController.getAll);
+router.get("/pessoas/:id",PessoasController.getByIdValidation,PessoasController.getById);
 
 export {router};
