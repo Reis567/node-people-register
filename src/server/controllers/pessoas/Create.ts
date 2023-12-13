22ca2dfe58645c301565d032a7b334328fd5f04d
Reis567 <matheusdosreis9@gmail.com>
2023-12-13 08:17:02 -0300

    create method on controllers

---
+++ src/server/controllers/pessoas/Create.ts
@@ -0,0 +1,34 @@
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
+import { validation } from "../../shared/middlewares";
+import { IPessoa } from "../../database/models";
+import { PessoasProvider } from "../../database/providers/pessoas";
+
+const bodyValidation = yup.object({
+    nomeCompleto: yup.string().required().min(3),
+    email: yup.string().required().email(),
+    cidadeId: yup.number().required(),
+});
+
+interface IBodyProps extends Omit<IPessoa, 'id'> { }
+
+export const createValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(bodyValidation),
+}));
+
+export const create: RequestHandler = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
+    console.log(req.body);
+
+    const result = await PessoasProvider.create(req.body);
+
+    if (result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message,
+            },
+        });
+    }
+
+    return res.status(StatusCodes.CREATED).json(result);
+};
