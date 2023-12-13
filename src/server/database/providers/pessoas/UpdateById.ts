1b8f63da47a5defecd48b8108d4276aaca2eb8fc
Reis567 <matheusdosreis9@gmail.com>
2023-12-13 08:09:23 -0300

    error catching on pessoa relationship with cidadeId

--- src/server/database/providers/pessoas/UpdateById.ts
+++ src/server/database/providers/pessoas/UpdateById.ts
@@ -4,6 +4,13 @@ import { Knex } from '../../knex';
 
 export const updateById = async (id: number, pessoa: Omit<IPessoa, 'id'>): Promise<void | Error> => {
     try {
+        const[{count}] = await Knex(ETableNames.cidade)
+        .where('id','=',pessoa.cidadeId)
+        .count<[{count:number}]>('* as count');
+
+        if(count===0){
+            return new Error('A cidade usada no cadastro não foi encontrada')
+        }
         const result = await Knex(ETableNames.pessoa)
             .update(pessoa)
             .where('id', '=', id);
