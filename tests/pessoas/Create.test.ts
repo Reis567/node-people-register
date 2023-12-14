ac2d1b41159cd4e86ba4ff08b3458b7cc216e49d
Reis567 <matheusdosreis9@gmail.com>
2023-12-14 05:51:58 -0300

    create.test.ts test of pessoas com create

--- tests/pessoas/Create.test.ts
+++ tests/pessoas/Create.test.ts
@@ -1,2 +1,27 @@
 import { StatusCodes } from 'http-status-codes';
 import { testServer } from "../jest.setup";
+
+describe('Pessoas - Create', () => {
+    it('Cria registro', async () => {
+        const resposta1 = await testServer.post('/pessoas').send({
+            nomeCompleto: 'Fulano de Tal',
+            email: 'fulano@example.com',
+            cidadeId: 1, 
+        });
+
+        expect(resposta1.statusCode).toEqual(StatusCodes.CREATED);
+        expect(typeof resposta1.body).toEqual('number');
+    });
+
+    it('Tenta criar registro com nome curto/Cidade Id incorreto', async () => {
+        const resposta1 = await testServer.post('/pessoas').send({
+            nomeCompleto: 'Jo',
+            email: 'jo@example.com',
+            cidadeId: 9999,
+        });
+
+        expect(resposta1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(resposta1.body).toHaveProperty('errors.body.nomeCompleto');
+    });
+
+});
