97c6ddbdf2020eba458127ba281ffc5ac9cbbe92
Reis567 <matheusdosreis9@gmail.com>
2023-12-15 08:39:53 -0300

    email field index

--- src/server/database/migrations/0002_create_usuario.ts
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex){
     return knex.schema.createTable(ETableNames.usuario,table=>{
        table.bigIncrements('id').primary().index();
        table.string('nome').notNullable().checkLength('>',3);
-       table.string('email').unique().notNullable().checkLength('>',6);
+       table.string('email').index().unique().notNullable().checkLength('>',6);
        table.string('senha').notNullable().checkLength('>',6);
        table.comment('Tabela usada para armazenar usuarios no sistema')
     }).then(()=>{
