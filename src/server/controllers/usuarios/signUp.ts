48834dce59110f9267c7aed6e18af751879168a0
Reis567 <matheusdosreis9@gmail.com>
2023-12-15 15:01:35 -0300

    sign up method controller

---
+++ src/server/controllers/usuarios/signUp.ts
@@ -0,0 +1,32 @@
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
+import { validation } from "../../shared/middlewares";
+import { IUsuario } from "../../database/models";
+import { UsuariosProvider } from "../../database/providers/usuarios";
+
+const bodyValidation = yup.object({
+    nome: yup.string().required().min(3),
+    email: yup.string().required().email(),
+    senha: yup.string().required().min(6), 
+});
+
+interface IBodyProps extends Omit<IUsuario, 'id'> { }
+
+export const signupValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(bodyValidation),
+}));
+
+export const signup: RequestHandler = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
+    const result = await UsuariosProvider.create(req.body);
+
+    if (result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message,
+            },
+        });
+    }
+
+    return res.status(StatusCodes.CREATED).json(result);
+};
