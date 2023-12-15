5ec7d33b189169373ff3b260d3dc8b2fbd886511
Reis567 <matheusdosreis9@gmail.com>
2023-12-15 06:34:16 -0300

    create a city before do the get all test

--- tests/pessoas/GetAll.test.ts
+++ tests/pessoas/GetAll.test.ts
@@ -3,11 +3,16 @@ import { StatusCodes } from 'http-status-codes';
 
 
 describe('Pessoas - GetAll', ()=>{
+    let cidadeId :number|undefined = undefined
+    beforeAll(async()=>{
+        const resCidade = await testServer.post('/cidades').send({nome:'Brumadinho'})
+        cidadeId= resCidade.body
+    })
     it('Busca todos os registros',async ()=>{
         const resposta1 = await testServer.post('/pessoas').send({
             nomeCompleto: 'Fulano de Tal',
-            email: 'fulano@example.com',
-            cidadeId: 1, 
+            email: 'fulanogetall@example.com',
+            cidadeId, 
         
         })
         expect(resposta1.statusCode).toEqual(StatusCodes.CREATED);
