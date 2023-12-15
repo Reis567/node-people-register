3e18640a7e49ebf72d9cf4e6dd8262caf20f4cc7
Reis567 <matheusdosreis9@gmail.com>
2023-12-15 07:06:01 -0300

    double email test in create

--- tests/pessoas/Create.test.ts
+++ tests/pessoas/Create.test.ts
@@ -2,31 +2,51 @@ import { StatusCodes } from 'http-status-codes';
 import { testServer } from "../jest.setup";
 
 describe('Pessoas - Create', () => {
-    let cidadeId :number|undefined = undefined
-    beforeAll(async()=>{
-        const resCidade = await testServer.post('/cidades').send({nome:'Brumadinho'})
-        cidadeId= resCidade.body
-    })
+    let cidadeId: number | undefined = undefined;
+
+    beforeAll(async () => {
+        const resCidade = await testServer.post('/cidades').send({ nome: 'Brumadinho' });
+        cidadeId = resCidade.body;
+    });
+
     it('Cria registro', async () => {
         const resposta1 = await testServer.post('/pessoas').send({
             nomeCompleto: 'Fulano de Tal',
-            email: 'fulano@example.com',
-            cidadeId, 
+            email: 'fulanoCreate1@example.com',
+            cidadeId,
         });
 
         expect(resposta1.statusCode).toEqual(StatusCodes.CREATED);
         expect(typeof resposta1.body).toEqual('number');
     });
-
-    it('Tenta criar registro com nome curto/Cidade Id incorreto', async () => {
+    it('Cria registro 2', async () => {
         const resposta1 = await testServer.post('/pessoas').send({
-            nomeCompleto: 'Jo',
-            email: 'jo@example.com',
-            cidadeId: 9999,
+            nomeCompleto: 'Fulano o segundo',
+            email: 'fulanoCreate2@example.com',
+            cidadeId,
         });
 
-        expect(resposta1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-        expect(resposta1.body).toHaveProperty('errors.body.nomeCompleto');
+        expect(resposta1.statusCode).toEqual(StatusCodes.CREATED);
+        expect(typeof resposta1.body).toEqual('number');
     });
+    it('Tenta criar registro com email duplicado', async () => {
+        const respostaemail1 = await testServer.post('/pessoas').send({
+            nomeCompleto: 'Ciclano de Tal',
+            email: 'fulanoCreate2@example.com', 
+            cidadeId,
+        });
+
+        expect(respostaemail1.statusCode).toEqual(StatusCodes.CREATED);
+
+        const respostaemail2 = await testServer.post('/pessoas').send({
+            nomeCompleto: 'Ciclano de Tal2',
+            email: 'fulanoCreate2@example.com', 
+            cidadeId,
+        });
+
+        expect(respostaemail2.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+        expect(respostaemail2.body).toHaveProperty('errors.default')
+    });
+
 
 });
