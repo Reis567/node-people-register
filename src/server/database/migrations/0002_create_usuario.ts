09c454aca0d906d867fae623f05b103eae1b63e5
Reis567 <matheusdosreis9@gmail.com>
2023-12-16 10:46:31 -0300

    lenght of users table

--- src/server/database/migrations/0002_create_usuario.ts
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -5,9 +5,9 @@ import { ETableNames } from "../ETableNames";
 export async function up(knex: Knex){
     return knex.schema.createTable(ETableNames.usuario,table=>{
        table.bigIncrements('id').primary().index();
-       table.string('nome').notNullable().checkLength('>',3);
-       table.string('email').index().unique().notNullable().checkLength('>',6);
-       table.string('senha').notNullable().checkLength('>',6);
+       table.string('nome').notNullable().checkLength('>=',3);
+       table.string('email').index().unique().notNullable().checkLength('>=',6);
+       table.string('senha').notNullable().checkLength('>=',6);
        table.comment('Tabela usada para armazenar usuarios no sistema')
     }).then(()=>{
         console.log(`# Create Table ${ETableNames.usuario}`)
