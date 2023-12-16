dff882cd18640dba322c14e915163b47a7ea6d94
Reis567 <matheusdosreis9@gmail.com>
2023-12-16 20:36:25 -0300

    test - try to login with short password

--- tests/usuarios/signIn.test.ts
+++ tests/usuarios/signIn.test.ts
@@ -48,4 +48,14 @@ describe('Usuarios - Signin', () => {
         expect(resposta2.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
         expect(resposta2.body).toHaveProperty('errors.default', 'Credenciais inválidas');
     });
+
+    it('Tenta autenticar usuário com senha curta', async () => {
+        const resposta2 = await testServer.post('/entrar').send({
+            email: 'test@example.com',
+            senha: 's',
+        });
+
+        expect(resposta2.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+        expect(resposta2.body).toHaveProperty('errors.default', 'Credenciais inválidas');
+    });
 });
