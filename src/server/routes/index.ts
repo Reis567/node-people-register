12560ed8df74ae38c46cfafed2e20278944d1ad0
Reis567 <matheusdosreis9@gmail.com>
2023-12-17 11:35:52 -0300

    passing in all the routes the authentication

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {StatusCodes} from "http-status-codes"
 import { CidadesController, PessoasController } from "../controllers";
 import { UsuariosController } from "../controllers/usuarios";
+import { ensureAuthenticated } from "../shared/middlewares";
 
 const router = Router()
 
@@ -11,26 +12,26 @@ router.get("/",)
 // Rotas de Cidades
 
 //Read routes
-router.get("/cidades",CidadesController.getAllValidation,CidadesController.getAll);
-router.get("/cidades/:id",CidadesController.getByIdValidation,CidadesController.getById);
+router.get("/cidades",ensureAuthenticated,CidadesController.getAllValidation,CidadesController.getAll);
+router.get("/cidades/:id",ensureAuthenticated,CidadesController.getByIdValidation,CidadesController.getById);
 //Create routes
-router.post("/cidades",CidadesController.createValidation,CidadesController.create)
+router.post("/cidades",ensureAuthenticated,CidadesController.createValidation,CidadesController.create)
 //Update routes
-router.put("/cidades/:id",CidadesController.UpdateByIdValidation,CidadesController.UpdateById)
+router.put("/cidades/:id",ensureAuthenticated,CidadesController.UpdateByIdValidation,CidadesController.UpdateById)
 //Delete routes
-router.delete("/cidades/:id",CidadesController.DeleteByIdValidation,CidadesController.DeleteById)
+router.delete("/cidades/:id",ensureAuthenticated,CidadesController.DeleteByIdValidation,CidadesController.DeleteById)
 
 // Rotas de Pessoas
 
 //Read routes
-router.get("/pessoas",PessoasController.getAllValidation,PessoasController.getAll);
-router.get("/pessoas/:id",PessoasController.getByIdValidation,PessoasController.getById);
+router.get("/pessoas",ensureAuthenticated,PessoasController.getAllValidation,PessoasController.getAll);
+router.get("/pessoas/:id",ensureAuthenticated,PessoasController.getByIdValidation,PessoasController.getById);
 //Create routes
-router.post("/pessoas",PessoasController.createValidation,PessoasController.create)
+router.post("/pessoas",ensureAuthenticated,PessoasController.createValidation,PessoasController.create)
 //Update routes
-router.put("/pessoas/:id",PessoasController.updateByIdValidation,PessoasController.updateById)
+router.put("/pessoas/:id",ensureAuthenticated,PessoasController.updateByIdValidation,PessoasController.updateById)
 //Delete routes
-router.delete("/pessoas/:id",PessoasController.deleteByIdValidation,PessoasController.deleteById)
+router.delete("/pessoas/:id",ensureAuthenticated,PessoasController.deleteByIdValidation,PessoasController.deleteById)
 
 
 /// Rotas de autenticação
