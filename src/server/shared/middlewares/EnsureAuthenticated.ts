2205cb0ae1ec6ab81f218dd8d4108e07d755707a
Reis567 <matheusdosreis9@gmail.com>
2023-12-17 11:49:42 -0300

    checks if the header has the authorization token

--- src/server/shared/middlewares/EnsureAuthenticated.ts
+++ src/server/shared/middlewares/EnsureAuthenticated.ts
@@ -1,8 +1,18 @@
 import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
 
 
 
 export const ensureAuthenticated:RequestHandler = async (req,res,next)=> {
+    const {authorization} = req.headers
+
+    console.log(req.headers)
+
+    if(!authorization){
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            errors:{default:'Não autenticado'}
+        })
+    }
     
     return next();
 }
