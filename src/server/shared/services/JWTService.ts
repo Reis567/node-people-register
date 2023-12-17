56fe9cf3f71e563378d96cc1f89354d59a269ef6
Reis567 <matheusdosreis9@gmail.com>
2023-12-17 18:07:09 -0300

    return the decoded token

--- src/server/shared/services/JWTService.ts
+++ src/server/shared/services/JWTService.ts
@@ -14,13 +14,24 @@ const sign = (data:IJWTData)=>{
     return jwt.sign(data,process.env.JWT_SECRET,{expiresIn:'24h'})
 }
 
-const verify = (token:string) => {
+const verify = (token:string):IJWTData |'JWT_SECRET_NOT_FOUND'|'INVALID_TOKEN' => {
     if(!process.env.JWT_SECRET){
         return 'JWT_SECRET_NOT_FOUND'
     }
 
     try {
        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+       if( typeof decoded ==='string' ) {
+
+        return 'INVALID_TOKEN'
+
+       } else{
+
+        return decoded as IJWTData;
+       
+        }
+
     } catch (error) {
         console.log(error)
         return 'INVALID_TOKEN'
