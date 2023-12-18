eff1993770b1f5f824d5b29e4c05ba3a54336d8c
Reis567 <matheusdosreis9@gmail.com>
2023-12-18 07:18:04 -0300

    test - create login to get token

--- tests/pessoas/Create.test.ts
+++ tests/pessoas/Create.test.ts
@@ -2,6 +2,14 @@ import { StatusCodes } from 'http-status-codes';
 import { testServer } from "../jest.setup";
 
 describe('Pessoas - Create', () => {
+    let accessToken = '';
+    beforeAll(async()=>{
+        const email = 'pessoas-create@gmail.com';
+        await testServer.post('/cadastrar').send({nome:'teste',email,senha:'12345678'})
+        const signInRes = await testServer.post('/entrar').send({email,senha:'12345678'});
+
+        accessToken = signInRes.body.accessToken
+    })
     let cidadeId: number | undefined = undefined;
 
     beforeAll(async () => {
