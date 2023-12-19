2cb9070e08dab74763d34d9e524f86d442cabf40
Reis567 <matheusdosreis9@gmail.com>
2023-12-19 07:59:27 -0300

    cors bug fixed

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,11 @@ import express from "express"
 import './shared/services/TranslationYup'
 import { router } from "./routes";
 import 'dotenv/config'
+import cors from 'cors'
 
 const server = express();
+
+server.use(cors())
 
 server.use(express.json())
 server.use(router)
