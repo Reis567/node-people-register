6ec0a05f234d3e78c10f2ea6417e12715643c998
Reis567 <matheusdosreis9@gmail.com>
2023-12-19 08:16:30 -0300

    documentation of routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,45 +1,107 @@
 import { Router } from "express";
-import {StatusCodes} from "http-status-codes"
+import { StatusCodes } from "http-status-codes";
 import { CidadesController, PessoasController } from "../controllers";
 import { UsuariosController } from "../controllers/usuarios";
 import { ensureAuthenticated } from "../shared/middlewares";
 
-const router = Router()
+const router = Router();
 
-//Base routes
-router.get("/",)
+// Rota base
+/**
+ * @route GET /
+ * @description Get information about the API
+ * @access Public
+ */
+router.get("/", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "API is running" });
+});
 
 // Rotas de Cidades
+/**
+ * @route GET /cidades
+ * @description Get all cities
+ * @access Private (requires authentication)
+ */
+router.get("/cidades", ensureAuthenticated, CidadesController.getAllValidation, CidadesController.getAll);
 
-//Read routes
-router.get("/cidades",ensureAuthenticated,CidadesController.getAllValidation,CidadesController.getAll);
-router.get("/cidades/:id",ensureAuthenticated,CidadesController.getByIdValidation,CidadesController.getById);
-//Create routes
-router.post("/cidades",ensureAuthenticated,CidadesController.createValidation,CidadesController.create)
-//Update routes
-router.put("/cidades/:id",ensureAuthenticated,CidadesController.UpdateByIdValidation,CidadesController.UpdateById)
-//Delete routes
-router.delete("/cidades/:id",ensureAuthenticated,CidadesController.DeleteByIdValidation,CidadesController.DeleteById)
+/**
+ * @route GET /cidades/:id
+ * @description Get city by ID
+ * @access Private (requires authentication)
+ */
+router.get("/cidades/:id", ensureAuthenticated, CidadesController.getByIdValidation, CidadesController.getById);
+
+/**
+ * @route POST /cidades
+ * @description Create a new city
+ * @access Private (requires authentication)
+ */
+router.post("/cidades", ensureAuthenticated, CidadesController.createValidation, CidadesController.create);
+
+/**
+ * @route PUT /cidades/:id
+ * @description Update city by ID
+ * @access Private (requires authentication)
+ */
+router.put("/cidades/:id", ensureAuthenticated, CidadesController.UpdateByIdValidation, CidadesController.UpdateById);
+
+/**
+ * @route DELETE /cidades/:id
+ * @description Delete city by ID
+ * @access Private (requires authentication)
+ */
+router.delete("/cidades/:id", ensureAuthenticated, CidadesController.DeleteByIdValidation, CidadesController.DeleteById);
 
 // Rotas de Pessoas
+/**
+ * @route GET /pessoas
+ * @description Get all people
+ * @access Private (requires authentication)
+ */
+router.get("/pessoas", ensureAuthenticated, PessoasController.getAllValidation, PessoasController.getAll);
 
-//Read routes
-router.get("/pessoas",ensureAuthenticated,PessoasController.getAllValidation,PessoasController.getAll);
-router.get("/pessoas/:id",ensureAuthenticated,PessoasController.getByIdValidation,PessoasController.getById);
-//Create routes
-router.post("/pessoas",ensureAuthenticated,PessoasController.createValidation,PessoasController.create)
-//Update routes
-router.put("/pessoas/:id",ensureAuthenticated,PessoasController.updateByIdValidation,PessoasController.updateById)
-//Delete routes
-router.delete("/pessoas/:id",ensureAuthenticated,PessoasController.deleteByIdValidation,PessoasController.deleteById)
+/**
+ * @route GET /pessoas/:id
+ * @description Get person by ID
+ * @access Private (requires authentication)
+ */
+router.get("/pessoas/:id", ensureAuthenticated, PessoasController.getByIdValidation, PessoasController.getById);
 
+/**
+ * @route POST /pessoas
+ * @description Create a new person
+ * @access Private (requires authentication)
+ */
+router.post("/pessoas", ensureAuthenticated, PessoasController.createValidation, PessoasController.create);
+
+/**
+ * @route PUT /pessoas/:id
+ * @description Update person by ID
+ * @access Private (requires authentication)
+ */
+router.put("/pessoas/:id", ensureAuthenticated, PessoasController.updateByIdValidation, PessoasController.updateById);
+
+/**
+ * @route DELETE /pessoas/:id
+ * @description Delete person by ID
+ * @access Private (requires authentication)
+ */
+router.delete("/pessoas/:id", ensureAuthenticated, PessoasController.deleteByIdValidation, PessoasController.deleteById);
 
 /// Rotas de autenticação
 
-//Register
-router.post('/cadastrar',UsuariosController.signupValidation,UsuariosController.signup)
+/**
+ * @route POST /cadastrar
+ * @description Register a new user
+ * @access Public
+ */
+router.post('/cadastrar', UsuariosController.signupValidation, UsuariosController.signup);
 
-//Login 
-router.post('/entrar',UsuariosController.signinValidation,UsuariosController.signin)
+/**
+ * @route POST /entrar
+ * @description Login
+ * @access Public
+ */
+router.post('/entrar', UsuariosController.signinValidation, UsuariosController.signin);
 
-export {router};
+export { router };
